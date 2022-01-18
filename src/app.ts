c9f43c5dc76b740c44e15585f852f092c2c81921
odan <odan3240@gmail.com>
2022-01-19 00:02:17 +0900

    web3.eth.getAccounts を使えるようにする

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,34 @@
 import { JsonRpcEngine } from "json-rpc-engine";
 import {
   createFetchMiddleware,
+  createWalletMiddleware,
   providerFromEngine,
 } from "eth-json-rpc-middleware";
+import { publicKeyCreate } from "secp256k1";
+import create from "keccak";
 
 import Web3 from "web3";
 
-function createSampleProvider(rpcUrl: string) {
+function createSampleProvider(rpcUrl: string, privateKey: string) {
   const engine = new JsonRpcEngine();
+
+  engine.push(
+    createWalletMiddleware({
+      getAccounts: async () => {
+        const publicKey = publicKeyCreate(
+          Buffer.from(privateKey.substring(2), "hex"),
+          false
+        ).slice(1);
+
+        const address = create("keccak256")
+          .update(Buffer.from(publicKey))
+          .digest()
+          .slice(12, 32);
+
+        return Promise.resolve([`0x${address.toString("hex")}`]);
+      },
+    })
+  );
 
   engine.push(createFetchMiddleware({ rpcUrl }));
 
@@ -16,16 +37,21 @@ function createSampleProvider(rpcUrl: string) {
 
 async function main(): Promise<void> {
   const rpcUrl = process.env.RINKEBY_URL;
+  const privateKey = process.env.PRIVATE_KEY;
 
   if (!rpcUrl) {
     throw new Error("RINKEBY_URL is undefiend");
   }
+  if (!privateKey) {
+    throw new Error("PRIVATE_KEY is undefiend");
+  }
 
-  const sampleProvider = createSampleProvider(rpcUrl);
+  const sampleProvider = createSampleProvider(rpcUrl, privateKey);
 
   const web3 = new Web3(sampleProvider as any);
 
   console.log(await web3.eth.getBlockNumber());
+  console.log(await web3.eth.getAccounts());
 }
 
 main();
